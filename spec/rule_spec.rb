83f13a7698b255dff359884ab03d459304c47124
Long Tran <long.tran@vsource.io>
2019-01-24 06:33:02 +0700

    use association and validation

--- spec/rule_spec.rb
+++ spec/rule_spec.rb
@@ -1,11 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Rule, :type => :model do
-  it 'is valid with valid attributes' do
+  subject {
     u = User.new(username: 'long', password: '123')
     r = Rule.new(cidr: '192.168.1.0/24', permission: 'allow')
     r.user = u
-    expect(r).to be_valid
+  }
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
   end
 
   it 'it not valid without a proper cird' do
@@ -18,8 +21,14 @@ RSpec.describe Rule, :type => :model do
   it 'is not valid with wrong permission' do
     u = User.new(username: 'long', password: '123')
     r = Rule.new(cidr: '192.168.1.0/24')
+    r.permission = :allow
     r.user = u
-    r.permission = nil
-    expect(r).to_not be_valid
+    expect(r).to be_valid
+  end
+
+  it 'is valid as belonging to User' do
+    @user = User.create(username: 'long', password: '123')
+    @rule = @user.rules.create(cidr: '192.168.1.0/23', permission: :allow)
+    expect(@rule).to be_valid
   end
 end
