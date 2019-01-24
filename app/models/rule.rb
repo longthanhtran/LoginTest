06d2195aa9dec835ff4729efa23eb36453931572
Long Tran <long.thanh.tran@gmail.com>
2019-01-24 08:13:11 +0700

    Finish Rule and User implementation

--- app/models/rule.rb
+++ app/models/rule.rb
@@ -2,7 +2,5 @@ class Rule < ApplicationRecord
   belongs_to :user
   enum permission: [:allow, :deny]
 
-  # include ActiveModel::Validations
-  # validates_with CidrValidator, presence: { strict: true }
   validates :cidr, format: { with: /\A(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\/(3[0-2]|[1-2][0-9]|[0-9]))\Z/, messages: "Invalid CIDR"}
 end
--- spec/rule_spec.rb
+++ spec/rule_spec.rb
@@ -12,18 +12,16 @@ RSpec.describe Rule, :type => :model do
   end
 
   it 'it not valid without a proper cird' do
-    u = User.new(username: 'long', password: '123')
-    r = Rule.new(cidr: '192.168.1.0/33')
-    r.user = u
-    expect(r).to_not be_valid
+    @user = User.create(username: 'long', password: '123')
+    @rule = @user.rules.create(cidr: '192.168.1.0/33', permission: :allow)
+    expect(@rule).to_not be_valid
   end
 
   it 'is not valid with wrong permission' do
-    u = User.new(username: 'long', password: '123')
-    r = Rule.new(cidr: '192.168.1.0/24')
-    r.permission = :allow
-    r.user = u
-    expect(r).to be_valid
+    @user = User.create(username: 'long', password: '123')
+    @rule = @user.rules.create(cidr: '192.168.1.0/23', permission: :allow)
+    @rule.permission = :allow
+    expect(@rule).to be_valid
   end
 
   it 'is valid as belonging to User' do
