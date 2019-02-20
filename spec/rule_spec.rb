56aed017ed06f1e9bc774c06f3f1503dc5944f52
Long Tran <long.tran@vsource.io>
2019-02-21 06:21:09 +0700

    use has_secure_password

--- spec/rule_spec.rb
+++ spec/rule_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Rule, :type => :model do
+
   subject {
-    u = User.new(username: 'long', password: '123')
+    u = User.new(username: 'long', password: '123456789')
     r = Rule.new(cidr: '192.168.1.0/24', permission: 'allow')
     r.user = u
   }
@@ -12,21 +13,22 @@ RSpec.describe Rule, :type => :model do
   end
 
   it 'it not valid without a proper cird' do
-    @user = User.create(username: 'long', password: '123')
+    @user = User.create(username: 'long', password: '123456789')
     @rule = @user.rules.create(cidr: '192.168.1.0/33', permission: :allow)
     expect(@rule).to_not be_valid
   end
 
   it 'is not valid with wrong permission' do
-    @user = User.create(username: 'long', password: '123')
+    @user = User.create(username: 'long', password: '123456789')
     @rule = @user.rules.create(cidr: '192.168.1.0/23', permission: :allow)
     @rule.permission = :allow
-    expect(@rule).to be_valid
+    expect(@user).to be_valid
   end
 
   it 'is valid as belonging to User' do
-    @user = User.create(username: 'long', password: '123')
+    @user = User.create(username: 'long', password: '123456789')
     @rule = @user.rules.create(cidr: '192.168.1.0/23', permission: :allow)
     expect(@rule).to be_valid
   end
+
 end
