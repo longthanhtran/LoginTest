172af2ba9320673cb37460a6c111f9f3cec582ce
Long Tran <long.thanh.tran@gmail.com>
2019-08-17 07:26:50 +0700

    fix minor syntax per rubocop

--- spec/models/rule_spec.rb
+++ spec/models/rule_spec.rb
@@ -7,21 +7,21 @@ RSpec.describe Rule, type: :model do
 
       expect(user.rules).not_to eq(nil)
     end
-    
+
     it 'has CIDR valid attribute' do
       user = User.create!
       rule = user.rules.create!(cidr: '172.16.0.0/24')
 
       expect(rule.cidr).not_to eq(nil)
     end
-    
+
     it 'has either allow or deny permission' do
       user = User.create!
       rule = user.rules.create(permission: 'allow')
 
       expect(rule.permission).not_to eq(nil)
     end
-    
+
     it 'should validate against CIDR range and permission attribute' do
     end
   end
